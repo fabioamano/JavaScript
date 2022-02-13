61b6be991db8dcc53d9972985e3ed1475d9a1f28
fabioamano <henriquefabio205@gmail.com>
2022-02-13 14:48:07 -0300

    códigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+//tipo de dado
+//booleanos
+// '===' comparam o valor e seu tipo
+
+//conversao implicita
+const numero = 456;
+const numeroString = "456";
+
+//conversao explicita
+//number()
+//string()
+
+console.log(numero == numeroString)
+console.log(numero + Number(numeroString))
+
+
