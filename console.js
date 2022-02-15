bc14ef3aa25cfb13b7ba547d7ae89b84d398b5da
fabioamano <henriquefabio205@gmail.com>
2022-02-15 20:29:28 -0300

    javascript e nodejs

---
+++ console.js
@@ -0,0 +1,11 @@
+// log -> registro
+
+/* const minhaVar = true;
+
+console.log(245)
+console.log('eu sou umm texto')
+console.log(minhaVar) */
+
+// tratamento de erro
+
+console.error('deu erro!')
---
+++ erros.js
@@ -0,0 +1,10 @@
+//sem inicializador
+//const numero;
+const numero = 1;
+const minhaVar = 200;
+//problema com sintaxe
+//console.log(numero;
+console.log(numero);
+//erro de referência
+//console.log(minhaVar);
+console.log(minhaVar);
