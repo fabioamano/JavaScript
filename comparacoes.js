5c5865f9e51b8ffa7b29ea68ea01e43f5cc4ce78
fabioamano <henriquefabio205@gmail.com>
2022-02-15 22:00:52 -0300

    operadores

---
+++ comparacoes.js
@@ -0,0 +1,22 @@
+// == comparacao implicita
+
+const numero = 5;
+const texto = '5';
+
+console.log(numero == texto)
+
+//comparacao explicita 
+
+console.log(numero === texto)
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == compara só o valor
+// === compara o valor e o tipo do dados
+
+//conversão explicita
+
+Number()
+String()
---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = 'Fábio';
+const idade = 2022 - 1993;
+const cidadeDeNascimento = 'Conselheiro Lafaiete';
+
+//const apresentacao = 'Meu nome é ' + nome + ', minha idade é ' + idade + ' e nasci na cidade ';
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} anos e nasci na cidade de ${cidadeDeNascimento}`;
+console.log(apresentacao)
+
