fe872e64db93c12c0f5e7c94aded79621830db3c
fabioamano <henriquefabio205@gmail.com>
2022-02-22 22:24:19 -0300

    laços de repetição

---
+++ for.js
@@ -0,0 +1,7 @@
+const numeros = [100, 200, 300, 400, 500, 600]
+
+                // 6 (tamanho da array)
+for (let i = 0; 1 < numeros;length; i++) {
+               // [0]
+    console.log(1, numeros[i])
+}
